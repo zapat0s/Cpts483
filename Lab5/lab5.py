f0487cd4172df07054264dfe079494e48802030c
Joshua Clark <joshua.a.clark@email.wsu.edu>
2014-10-28 13:14:06 -0700

    Now gets an image from ROS

--- Lab5/lab5.py
+++ Lab5/lab5.py
@@ -7,53 +7,24 @@ import rospy
 import cv2
 import numpy as np
 
+from threading import Lock
+
 #  called "cmd_vel" using a message type "geometry_msgs/Twist"
+from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
+from cv_bridge import CvBridge, CvBridgeError
 
 
-class ARImageTacker:
-    def __init__(self):
-        # Setup Publishers and Subscibers
-        self.twistPublisher = rospy.Publisher('cmd_vel', Twist)
-        self.subVideo = rospy.Subscriber('/ardrone/front/image_raw', Image, self.ReceiveImage)
-
-        self.image = None
-        self.imageLock = Lock();
-
-        # Control Loop
-        while True:
-            cv_image = None
-            self.imageLock.acquire()
-            try:
-                if self.image is not None:    
-                    cv_img = ToOpenCV(self.image)
-            finally:
-                self.imageLock.release()
-            if cv_image is not None:
-                cv2.imshow('camera', cv_img)
-                cv2.waitKey(0)
-                cv2.destroyAllWindows()
-                (x, y) = BallLocation(cv_img)
-                if x - cv_image.width/2 < 10 and x - cv_image.width/2 >  -10: # Ball is centered hhorizontaly
-                    if y - cv_image.height/2 < 10 and y - cv_image.height/2 > -10: # Ball is centered verticaly            
-		        # fly forward
-                        print "Flying forward"
-                    else:
-                        # change altitude
-                        print "Changing altitude"
-                else:
-                    # rotate
-                    print "Rotating"
-
-    def ToOpenCV(ros_image):
+class ARImageTracker:
+    def ToOpenCV(self, ros_image):
         try:
-            cv_image = bridge.imgmsg_to_cv(ros_image, "bgr8")
+            cv_image = self.bridge.imgmsg_to_cv(ros_image, "bgr8")
             return cv_image
         except CvBridgeError, e:
             print e
             raise Exception("Failed to convert to OpenCV image")
 
-    def BallLocation(cv_image):
+    def BallLocation(self, cv_image):
         # Convert image to hsv
         img_hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
 
@@ -86,9 +57,42 @@ class ARImageTacker:
         finally:
             self.imageLock.release()
 
+    def __init__(self):
+        # Setup Publishers and Subscibers
+        self.twistPublisher = rospy.Publisher('cmd_vel', Twist)
+        self.subVideo = rospy.Subscriber('/ardrone/image_raw', Image, self.ReceiveImage) # try /ardrone/image_raw
+
+        self.image = None
+        self.imageLock = Lock()
+
+        self.bridge = CvBridge()
+        # Control Loop
+        while True:
+            cv_image = None
+            self.imageLock.acquire()
+            try:
+                if self.image is not None:
+                    cv_img = np.asarray(self.ToOpenCV(self.image))
+            finally:
+                self.imageLock.release()
+            if cv_image is not None:
+                (x, y) = self.BallLocation(cv_img)
+                if x - cv_image.width/2 < 10 and x - cv_image.width/2 >  -10: # Ball is centered hhorizontaly
+                    if y - cv_image.height/2 < 10 and y - cv_image.height/2 > -10: # Ball is centered verticaly            
+		        # fly forward
+                        print "Flying forward"
+                    else:
+                        # change altitude
+                        print "Changing altitude"
+                else:
+                    # rotate
+                    print "Rotating"
+
 # main file -- if we "import move" in another file, this code will not execute.
 if __name__== "__main__":
     # first thing, init a node!
     rospy.init_node('lab5')
+
+    node = ARImageTracker()
 
     
