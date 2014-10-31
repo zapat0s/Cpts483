81279c7b6a54745adb61198aaaf9a6cfdb81eada
Joshua Clark <joshua.a.clark@email.wsu.edu>
2014-10-30 20:38:27 -0700

    Lab 5 now works. Still unable to take off.

--- Lab5/lab5.py
+++ Lab5/lab5.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
 #Nick Hayward
 #Joshua Clark
-#Lab 4
+#Josh Woldstad
+#Andrew Kim
+#Lab 5
 
-import rospy
-import cv2
+import rospy, cv2, math
 import numpy as np
 
 from threading import Lock
 
-#  called "cmd_vel" using a message type "geometry_msgs/Twist"
+from std_msgs.msg import Empty
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge, CvBridgeError
 
 
 class ARImageTracker:
+    # Converts from ROS image to OpenCV Image
     def ToOpenCV(self, ros_image):
         try:
             cv_image = self.bridge.imgmsg_to_cv(ros_image, "bgr8")
@@ -24,6 +26,7 @@ class ARImageTracker:
             print e
             raise Exception("Failed to convert to OpenCV image")
 
+    # Locates center of Orange Ball
     def BallLocation(self, cv_image):
         # Convert image to hsv
         img_hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
@@ -43,13 +46,14 @@ class ARImageTracker:
         for c in contours:
             if len(c) > tar_contour_length:
                 tar_contour = c
-                tar_contour_length = len(c)      
-        print tar_contour_length
+                tar_contour_length = len(c)
 
         (x,y),radius = cv2.minEnclosingCircle(tar_contour)
         center = (int(x),int(y))
         return center;
 
+    # Sets image data
+    # Only called by Subscriber Callback
     def ReceiveImage(self, data):
         self.imageLock.acquire()
         try:
@@ -59,6 +63,7 @@ class ARImageTracker:
 
     def __init__(self):
         # Setup Publishers and Subscibers
+        self.takeoffPublisher = rospy.Publisher('/ardrone/takeoff', Empty) 
         self.twistPublisher = rospy.Publisher('cmd_vel', Twist)
         self.subVideo = rospy.Subscriber('/ardrone/image_raw', Image, self.ReceiveImage) # try /ardrone/image_raw
 
@@ -66,27 +71,51 @@ class ARImageTracker:
         self.imageLock = Lock()
 
         self.bridge = CvBridge()
+
+        # Takeoff DOESNT WORK
+        #empty_msg = Empty()
+        #self.takeoffPublisher.publish(empty_msg)
+
         # Control Loop
-        while True:
-            cv_image = None
+        while not rospy.is_shutdown():
+            cv_img = None
             self.imageLock.acquire()
             try:
                 if self.image is not None:
                     cv_img = np.asarray(self.ToOpenCV(self.image))
             finally:
                 self.imageLock.release()
-            if cv_image is not None:
-                (x, y) = self.BallLocation(cv_img)
-                if x - cv_image.width/2 < 10 and x - cv_image.width/2 >  -10: # Ball is centered hhorizontaly
-                    if y - cv_image.height/2 < 10 and y - cv_image.height/2 > -10: # Ball is centered verticaly            
-		        # fly forward
-                        print "Flying forward"
-                    else:
-                        # change altitude
-                        print "Changing altitude"
-                else:
-                    # rotate
-                    print "Rotating"
+            if cv_img is not None:
+                # Calculate angle 
+                fov = 46.0
+                ball_x = ball_y = 0
+                try:
+                    (ball_x, ball_y) = self.BallLocation(cv_img)
+                except cv2.error, e:
+                    # Rotate to Find Ball
+                    twist_msg = Twist()
+                    twist_msg.angular.z = 5.0
+                    self.twistPublisher.publish(twist_msg)
+                    print "searching"
+                    continue
+                height, width, depth = cv_img.shape
+                vertical_dif = (height / 2) - ball_y
+                horizontal_dif = (width / 2) - ball_x
+                vertical_angle = fov / (height / 2) * vertical_dif
+                horizontal_angle = fov / (width / 2) * horizontal_dif
+                # Create Twist Message
+                twist_msg = Twist()
+                twist_msg.linear.x = 1.0
+                twist_msg.linear.y = math.sin(math.radians(horizontal_angle))
+                twist_msg.linear.z = math.sin(math.radians(vertical_angle))
+                self.twistPublisher.publish(twist_msg)
+                # Uncomment to Draw center and show image
+                #cv2.circle(cv_img, (ball_x, ball_y), 3,(0,255,0),2)
+                #cv2.imshow('camera', cv_img)
+                #cv2.waitKey(0)
+                #cv2.destroyAllWindows()
+                
+                
 
 # main file -- if we "import move" in another file, this code will not execute.
 if __name__== "__main__":
